b69306f2a5322645defcf338cd2211232b250f7c
Zengotim <zengotim@yahoo.com>
2021-03-08 16:08:53 -0500

    Texture section completed

--- OpenGL_basic_tut/first.fsh
+++ OpenGL_basic_tut/first.fsh
@@ -1,16 +1,13 @@
 #version 330 core
-
 out vec4 FragColor;
+  
 in vec3 ourColor;
-uniform vec4 colorMod;
+in vec2 TexCoord;
+
+uniform sampler2D texture1;
+uniform sampler2D texture2;
 
 void main()
 {
-   //float red, blue, green;
-   //red = colorMod.x; //mod((ourColor.x + colorMod.x/2.0f), 1.0f);
-   //green = colorMod.y; //mod((ourColor.z + colorMod.y/2.0f), 1.0f);
-   //blue = colorMod.z; //mod((ourColor.y + colorMod.z/2.0f), 1.0f);
-   
-
-   FragColor = colorMod;
-};
+    FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2);
+}
--- OpenGL_basic_tut/threedsys_main.cpp
+++ OpenGL_basic_tut/threedsys_main.cpp
@@ -1,8 +1,10 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include "GLHelper.h"
-#include "Shader.h"
+#include "shader.h"
+#include "image_loader.h"
 
+#include <fstream>
 #include <iostream>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
@@ -16,10 +18,11 @@ const unsigned int SCR_HEIGHT = 600;
 
 
 float vertices[] = {
-     0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 0.0f, // top right
-     0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, // bottom right
-    -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, // bottom left
-    -0.5f,  0.5f, 0.0f,  1.0f, 0.0f, 0.0f // top left 
+    //positions         //colors           //tex coords
+     0.5f,  0.5f, 0.0f,  1.0f, 0.0f, 0.0f, 1.0f, 1.0f,// top right
+     0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f, 1.0f, 0.0f,// bottom right
+    -0.5f, -0.5f, 0.0f,  0.0f, 0.0f, 1.0f, 0.0f, 0.0f,// bottom left
+    -0.5f,  0.5f, 0.0f,  1.0f, 0.0f, 0.0f, 0.0f, 1.0f// top left 
 };
 unsigned int indices[] = {  // note that we start from 0!
     0, 1, 3,  // first Triangle
@@ -51,11 +54,14 @@ int main()
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     // position attrib
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
     // color attrib
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
+    //tex attrib
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
 
     // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
     glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -71,6 +77,53 @@ int main()
     // uncomment this call to draw in wireframe polygons.
     //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
+
+
+    //***** Texture section ******
+    stbi_set_flip_vertically_on_load(true);
+
+    unsigned int texture1,texture2;
+    glGenTextures(1, &texture1);
+    glBindTexture(GL_TEXTURE_2D, texture1);
+    //Wrapping and filtering options
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    // Load and generate
+    int width, height, nrChannels;
+    unsigned char* data = stbi_load("container.jpg", &width, &height, &nrChannels, 0);
+
+    if (data) {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else {
+        std::cout << "Failed to load texture!" << std::endl;
+    }
+    stbi_image_free(data);
+
+    glGenTextures(1, &texture2);
+    glBindTexture(GL_TEXTURE_2D, texture2);
+    //Wrapping and filtering options
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    data = stbi_load("awesomeface.png", &width, &height, &nrChannels, 0);
+    if (data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    stbi_image_free(data);
+
+    firstShader.use();
+    glUniform1i(glGetUniformLocation(firstShader.ID, "texture1"), 0); //manual set
+    firstShader.setInt("texture2", 1); //class set
+    //int vertexColorLocation = glGetUniformLocation(firstShader.ID, "colorMod");
+
+
     // render loop
     // -----------
     while (!glfwWindowShouldClose(window))
@@ -85,21 +138,22 @@ int main()
         glClear(GL_COLOR_BUFFER_BIT);
         
 
-        float timeValue = glfwGetTime();
-        float redValue = (sin(timeValue) / 2.0f) + 0.5f;
-        float greenValue = cos(timeValue) / 2.0f + 0.5f;
-        float blueValue = cos(timeValue) / 2.0f + 0.5f;
+        //float timeValue = glfwGetTime();
+        //float redValue = (sin(timeValue) / 2.0f) + 0.5f;
+        //float greenValue = cos(timeValue) / 2.0f + 0.5f;
+        //float blueValue = cos(timeValue) / 2.0f + 0.5f;
+
+
+
+        //glUniform4f(vertexColorLocation, redValue, greenValue, blueValue, 1.0f);
+        //firstShader.setFloat("vertexColorLocation", redValue);
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture1);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, texture2);
 
         firstShader.use();
-
-        int vertexColorLocation = glGetUniformLocation(firstShader.ID, "colorMod");
-        std::cout << "Color is " << redValue << ", " << greenValue << ", " << blueValue << std::endl;
-
-        glUniform4f(vertexColorLocation, redValue, greenValue, blueValue, 1.0f);
-        //firstShader.setFloat("vertexColorLocation", redValue);
-
-        glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-        //glDrawArrays(GL_TRIANGLES, 0, 6);
+        glBindVertexArray(VAO);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         // glBindVertexArray(0); // no need to unbind it every time 
 
